d557eea7cfd25dc4bd09131baef15d79af1f5bf1
Ryan O'Connor <rocsteady888@gmail.com>
2019-12-12 10:20:10 -0500

    collection-preview and collection-item initial commit'

--- src/index.js
+++ src/index.js
@@ -5,4 +5,4 @@ import { BrowserRouter } from "react-router-dom";
 
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('root'));
