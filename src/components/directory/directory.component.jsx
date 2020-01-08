630837837bb0012763494b8b0b2ceecbdb5e3393
Ryan O'Connor <rocsteady888@gmail.com>
2020-01-08 13:51:21 -0500

    added redux

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -47,14 +47,13 @@ class Directory extends React.Component {
   }
   render() {
     return (
-
-		<div className="directory-menu">
-      {
-        this.state.sections.map(({id, ...otherSectionProps }) => (
-          <MenuItem key={id} {...otherSectionProps}/>
-        ))
-      }
-    </div>
+      <div className="directory-menu">
+        {
+          this.state.sections.map(({id, ...otherSectionProps }) => (
+            <MenuItem key={id} {...otherSectionProps}/>
+          ))
+        }
+      </div>
     )
   }
 }
